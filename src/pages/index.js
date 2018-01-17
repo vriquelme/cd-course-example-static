6f841beb230cdc5cd3ee8b253bf6cd91896eeebf
vriquelme <31777577+vriquelme@users.noreply.github.com>
2018-01-17 10:49:20 -0300

    Update.js
    Rollback

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-     Hola Mundo!</h1>
+    <h1>Hola Mundo!</h1>
     <p>Welcome to this static page example.</p>
     <p>Now we are going to deploy this site on Amazon S3.</p>
     <Link to="/1-intro">Let's do this!</Link>
